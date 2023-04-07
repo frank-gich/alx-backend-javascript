1e4a6f58e5c8a5b53abc23bf4f8372aaaac994b0
frank-gich <gichuhi5781@gmail.com>
2023-04-07 11:38:16 +0300

    my second changes on js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
-    // eslint-disable-next-line in code
     const task = true;
-    // eslint-disable-next-line in the code
     const task2 = false;
   }
+  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }
