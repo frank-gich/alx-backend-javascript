acb8e6be6bcba07b890662aa81ac1b9ed3ca795e
frank-gich <gichuhi5781@gmail.com>
2023-08-22 11:30:33 +0300

    this creates a function which reads a file and prints all associated group

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,32 +1,33 @@
-const fs = require('fs') 
+const fs = require('fs');
 
 const countStudents = (path) => {
-    if (!fs.existsSync(path)) throw new Error('Cannot load the database');
-   
-    const data = fs.readFileSync(path, 'utf8');
+  if (!fs.existsSync(path)) throw new Error('Cannot load the database');
 
-    const students = data.split('\n')
+  const data = fs.readFileSync(path, 'utf8');
 
+  // split where a new line exists
+  const students = data.split('\n')
+    // turn a row into an array by splitting by a ','
     .map((row) => row.split(','))
-
+    // skip first row
     .filter((row) => row.length === 4 && row[0] !== 'firstname')
-    //convert everything into objects 
+    // covert into objects
     .map((row) => ({
-        firstName: row[0],
-        lastName: row[1],
-        age: row[2],
-        field: row[3].replace('\r', ''),
+      firstName: row[0],
+      lastName: row[1],
+      age: row[2],
+      field: row[3].replace('\r', ''),
     }));
-
-const CS = students.filter((student) => student.field === 'CS')
+  // generate CS students
+  const CS = students.filter((student) => student.field === 'CS')
     .map((student) => student.firstName);
-
-const SWE = students.filter((student) => student.field === 'SWE')
+  // generate SWE students
+  const SWE = students.filter((student) => student.field === 'SWE')
     .map((student) => student.firstName);
-
-console.log(`Number of students: ${students.length}`);
-console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(',')}`);
-console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(',')}`);
-}
+  // print length and convert each into a string
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
+  console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+};
 
 module.exports = countStudents;
