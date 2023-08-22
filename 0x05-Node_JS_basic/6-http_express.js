40f431675ef8164818f2ff21a2e1e4d12786995c
frank-gich <gichuhi5781@gmail.com>
2023-08-22 22:08:46 +0300

    created a smaler http server using express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,16 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
+
 const port = 1245;
 
 app.get('/',(req, res) => {
   res.setHeader('Content-Type', 'test/plain');
   res.send('Hello Holberton School!');
-
 });
+
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app
