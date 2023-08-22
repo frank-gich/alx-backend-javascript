85aaf8a05b7a3f4cae4a1cff411b565c847064e4
frank-gich <gichuhi5781@gmail.com>
2023-08-22 12:32:32 +0300

    just created a smell server using node js http module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,17 @@
 const http = require('http');
 
-const HOST_NAME = '127.0.0.1'
-const PORT = 1245
+const PORT = 1245;
+const HOST_NAME = '127.0.0.1';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello Holberton School!')
-})
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${HOST_NAME}:${PORT}/`)
-})
+app.listen(PORT, HOST_NAME, () => {
+  console.log(`Server running at http://${HOST_NAME}:${PORT}`);
+});
 
 module.exports = app;
+curl localhost:1245/any_endpoint && echo 
